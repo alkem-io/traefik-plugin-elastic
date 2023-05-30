e115584772df37d33332afd8a32d44d690e4ef5c
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-05-30 11:35:03 +0300

    Vendored packages

---
+++ vendor/github.com/elastic/go-elasticsearch/v8/typedapi/types/indextemplatemapping.go
@@ -0,0 +1,91 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// Code generated from the elasticsearch-specification DO NOT EDIT.
+// https://github.com/elastic/elasticsearch-specification/tree/363111664e81786557afe06e68221018847b3676
+
+package types
+
+import (
+	"bytes"
+	"errors"
+	"io"
+
+	"encoding/json"
+)
+
+// IndexTemplateMapping type.
+//
+// https://github.com/elastic/elasticsearch-specification/blob/363111664e81786557afe06e68221018847b3676/specification/indices/put_index_template/IndicesPutIndexTemplateRequest.ts#L61-L70
+type IndexTemplateMapping struct {
+	Aliases   map[string]Alias `json:"aliases,omitempty"`
+	Lifecycle *DataLifecycle   `json:"lifecycle,omitempty"`
+	Mappings  *TypeMapping     `json:"mappings,omitempty"`
+	Settings  *IndexSettings   `json:"settings,omitempty"`
+}
+
+func (s *IndexTemplateMapping) UnmarshalJSON(data []byte) error {
+
+	dec := json.NewDecoder(bytes.NewReader(data))
+
+	for {
+		t, err := dec.Token()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+
+		switch t {
+
+		case "aliases":
+			if s.Aliases == nil {
+				s.Aliases = make(map[string]Alias, 0)
+			}
+			if err := dec.Decode(&s.Aliases); err != nil {
+				return err
+			}
+
+		case "lifecycle":
+			if err := dec.Decode(&s.Lifecycle); err != nil {
+				return err
+			}
+
+		case "mappings":
+			if err := dec.Decode(&s.Mappings); err != nil {
+				return err
+			}
+
+		case "settings":
+			if err := dec.Decode(&s.Settings); err != nil {
+				return err
+			}
+
+		}
+	}
+	return nil
+}
+
+// NewIndexTemplateMapping returns a IndexTemplateMapping.
+func NewIndexTemplateMapping() *IndexTemplateMapping {
+	r := &IndexTemplateMapping{
+		Aliases: make(map[string]Alias, 0),
+	}
+
+	return r
+}
